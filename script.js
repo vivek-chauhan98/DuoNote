b5cf7d1f445a1e951aa55b3e38f21cdade9b8d3b
Vivek Chauhan <vivekchauhan3597@gmail.com>
2025-03-12 23:08:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1176,10 +1176,6 @@ searchIcon2.addEventListener('click', () => {
     logo.style.display = 'none'
     view.style.display = 'none'
     mode.style.display = 'none'
-
-    setTimeout(() => {
-        searchInput.focus();
-    }, 10000);
 })
 
 searchBack.addEventListener('click', () => {
